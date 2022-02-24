79479d6bad7b654ddf6be309384c7a61fb5f27ce
Jody Castor <jdcastor@gmail.com>
2022-02-24 11:01:16 -0600

    env fix

--- bulldog_getting_started/settings.py
+++ bulldog_getting_started/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'django-insecure-1)^n62xz+kqk56_tij1_f&e1d7fat-w$3!n7qkxoq1*b$6e3xx'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
